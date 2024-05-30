dc90e8a74142803b33d6bb4504d6ac801f16ad09
Clara K <clara@cynthialabs.net>
2024-05-30 15:49:56 +0200

    Update build.sh

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -3,7 +3,7 @@
 # This script is used to build the project.
 
 cd ../
-rm app/release/app-release.apk
+rm app/release/UngoogledFindMy.apk
 gradlew build --warning-mode all
 java -jar scripts/signer.jar -a app/build/outputs/apk/release/app-release-unsigned.apk -o app/release/
 mv app/release/app-release-aligned-debugSigned.apk app/release/UngoogledFindMy.apk
